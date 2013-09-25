9b2322b69b21f488b94f099aea8c4ff59c186b8e
EastAgile <eastagile@EastAgiles-iMac.local>
2013-09-25 09:42:30 +0700

    Create blank project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyAssignment::Application.config.secret_token = '66c1414b3b7d9dc76a6499b6475304a84286a1da71872a3cc68131d9bf5f777fec3f236eeef478a733e4f7fad8078af369f3d835e36573d17adf637cc1cb5110'
