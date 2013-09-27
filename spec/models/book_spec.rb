6b10f0070222bfa080af1585a1a46ad3ee952323
EastAgile <eastagile@EastAgiles-iMac.local>
2013-09-27 18:01:59 +0700

    [#57464746]Testing books model using rspec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,4 +7,16 @@ describe Book do
   it 'should not be empty' do 
   	book.should be_valid
   end
+
+  it 'should price must be positice' do
+  	book.price = -1.0
+  	book.should_not be_valid
+  	book.price = 0.01
+  	book.should be_valid
+  end
+
+  it { should have_many(:comments)}
+
+  it { should have_many(:order_lines)}
+
 end
