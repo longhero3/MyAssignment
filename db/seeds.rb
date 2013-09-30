d330038104dfd62dcbc057d4ffeadaea7527e130
EastAgile <eastagile@EastAgiles-iMac.local>
2013-09-30 15:50:16 +0700

    [#57884344]adding book browse by category

--- db/seeds.rb
+++ db/seeds.rb
@@ -334,5 +334,111 @@ Category.create(
   :sort_order => 'title'
 )
 # . . .
+BookCategory.delete_all
+BookCategory.create(
+  :book_id => 8,
+  :category_id => 2)
+BookCategory.create(
+  :book_id => 9,
+  :category_id => 2)
+BookCategory.create(
+  :book_id => 11,
+  :category_id => 2)
+BookCategory.create(
+  :book_id => 1,
+  :category_id => 1)
+BookCategory.create(
+  :book_id => 2,
+  :category_id => 1)
+BookCategory.create(
+  :book_id => 3,
+  :category_id => 1)
+BookCategory.create(
+  :book_id => 8,
+  :category_id => 1)
+BookCategory.create(
+  :book_id => 6,
+  :category_id => 3)
+BookCategory.create(
+  :book_id => 7,
+  :category_id => 3)
+BookCategory.create(
+  :book_id => 10,
+  :category_id => 4)
+BookCategory.create(
+  :book_id => 11,
+  :category_id => 4)
+BookCategory.create(
+  :book_id => 12,
+  :category_id => 4)
+BookCategory.create(
+  :book_id => 13,
+  :category_id => 4)
+BookCategory.create(
+  :book_id => 14,
+  :category_id => 4)
+BookCategory.create(
+  :book_id => 15,
+  :category_id => 4)
+BookCategory.create(
+  :book_id => 11,
+  :category_id => 5)
+BookCategory.create(
+  :book_id => 12,
+  :category_id => 5)
+BookCategory.create(
+  :book_id => 13,
+  :category_id => 5)
+BookCategory.create(
+  :book_id => 14,
+  :category_id => 5)
+BookCategory.create(
+  :book_id => 15,
+  :category_id => 5)
+BookCategory.create(
+  :book_id => 1,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 2,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 3,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 4,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 5,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 6,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 7,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 8,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 9,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 10,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 11,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 12,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 13,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 14,
+  :category_id => 6)
+BookCategory.create(
+  :book_id => 15,
+  :category_id => 6)
 
 
